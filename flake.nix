03ceaae7c84ca1db1949ae8f33de073665ef0d45
Herman Zhukov <societyofbruh@gmail.com>
2024-02-04 11:23:26 +0300

    Remove unused templates from flake.nix

--- flake.nix
+++ flake.nix
@@ -61,124 +61,9 @@
 
     {
       templates = rec {
-        clojure = {
-          path = ./clojure;
-          description = "Clojure development environment";
-        };
-
-        csharp = {
-          path = ./csharp;
-          description = "C# development environment";
-        };
-
-        cue = {
-          path = ./cue;
-          description = "Cue development environment";
-        };
-
-        dhall = {
-          path = ./dhall;
-          description = "Dhall development environment";
-        };
-
-        elixir = {
-          path = ./elixir;
-          description = "Elixir development environment";
-        };
-
-        elm = {
-          path = ./elm;
-          description = "Elm development environment";
-        };
-
-        gleam = {
-          path = ./gleam;
-          description = "Gleam development environment";
-        };
-
-        go = {
-          path = ./go;
-          description = "Go (Golang) development environment";
-        };
-
-        hashi = {
-          path = ./hashi;
-          description = "HashiCorp DevOps tools development environment";
-        };
-
-        haskell = {
-          path = ./haskell;
-          description = "Haskell development environment";
-        };
-
-        java = {
-          path = ./java;
-          description = "Java development environment";
-        };
-
-        kotlin = {
-          path = ./kotlin;
-          description = "Kotlin development environment";
-        };
-
-        latex = {
-          path = ./latex;
-          description = "LaTeX development environment";
-        };
-
-        nickel = {
-          path = ./nickel;
-          description = "Nickel development environment";
-        };
-
-        nim = {
-          path = ./nim;
-          description = "Nim development environment";
-        };
-
         nix = {
           path = ./nix;
           description = "Nix development environment";
-        };
-
-        node = {
-          path = ./node;
-          description = "Node.js development environment";
-        };
-
-        opa = {
-          path = ./opa;
-          description = "Open Policy Agent development environment";
-        };
-
-        php = {
-          path = ./php;
-          description = "PHP development environment";
-        };
-
-        protobuf = {
-          path = ./protobuf;
-          description = "Protobuf development environment";
-        };
-
-        pulumi = {
-          path = ./pulumi;
-          description = "Pulumi development environment";
-        };
-
-        purescript = {
-          path = ./purescript;
-          description = "Purescript development environment";
-        };
-
-        python = {
-          path = ./python;
-          description = "Python development environment";
-        };
-
-        ruby = {
-          path = ./ruby;
-          description = "Ruby development environment";
         };
 
         rust = {
@@ -189,11 +74,6 @@
         rust-toolchain = {
           path = ./rust-toolchain;
           description = "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        };
-
-        scala = {
-          path = ./scala;
-          description = "Scala development environment";
         };
 
         shell = {
