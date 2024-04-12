86ab7dfa85b98780e4772f6563fbecdc2710433b
Herman Zhukov <societyofbruh@gmail.com>
2024-04-12 10:42:06 +0300

    Improve rust flake, format and update all flakes

--- flake.nix
+++ flake.nix
@@ -1,15 +1,18 @@
 {
-  description =
-    "Ready-made templates for easily creating flake-driven environments";
+  description = "Ready-made templates for easily creating flake-driven environments";
 
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
 
-  outputs = { self, nixpkgs }:
+  outputs =
+    { self, nixpkgs }:
     let
       overlays = [
-        (final: prev:
-          let exec = pkg: "${prev.${pkg}}/bin/${pkg}";
-          in {
+        (
+          final: prev:
+          let
+            exec = pkg: "${prev.${pkg}}/bin/${pkg}";
+          in
+          {
             format = prev.writeScriptBin "format" ''
               ${exec "nixpkgs-fmt"} **/*.nix
             '';
@@ -32,75 +35,92 @@
                 (
                   cd $dir
                   ${exec "nix"} flake update # Update flake.lock
-                  ${
-                    exec "nix"
-                  } flake check  # Make sure things work after the update
+                  ${exec "nix"} flake check  # Make sure things work after the update
                 )
               done
             '';
-          })
+          }
+        )
       ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell { packages = with pkgs; [ format update ]; };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import nixpkgs { inherit overlays system; }; }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              format
+              update
+            ];
+          };
+        }
+      );
 
-      packages = forEachSupportedSystem ({ pkgs }: rec {
-        default = dvt;
-        inherit (pkgs) dvt;
-      });
+      packages = forEachSupportedSystem (
+        { pkgs }:
+        rec {
+          default = dvt;
+          inherit (pkgs) dvt;
+        }
+      );
     }
 
     //
 
-    {
-      templates = rec {
+      {
+        templates = rec {
 
-        default = empty;
+          default = empty;
 
-        empty = {
-          path = ./empty;
-          description = "Empty development environment";
+          empty = {
+            path = ./empty;
+            description = "Empty development environment";
+          };
+
+          c = {
+            path = ./c;
+            description = "C development environment";
+          };
+
+          nix = {
+            path = ./nix;
+            description = "Nix development environment";
+          };
+
+          rust = {
+            path = ./rust;
+            description = "Rust development environment";
+          };
+
+          #rust-toolchain = {
+          #  path = ./rust-toolchain;
+          #  description =
+          #    "Rust development environment with Rust version defined by a rust-toolchain.toml file";
+          #};
+
+          shell = {
+            path = ./shell;
+            description = "Shell script development environment";
+          };
+
+          zig = {
+            path = ./zig;
+            description = "Zig development environment";
+          };
+
+          # Aliases
+          # rt = rust-toolchain;
         };
-
-        c = {
-          path = ./c;
-          description = "C development environment";
-        };
-
-        nix = {
-          path = ./nix;
-          description = "Nix development environment";
-        };
-
-        rust = {
-          path = ./rust;
-          description = "Rust development environment";
-        };
-
-        #rust-toolchain = {
-        #  path = ./rust-toolchain;
-        #  description =
-        #    "Rust development environment with Rust version defined by a rust-toolchain.toml file";
-        #};
-
-        shell = {
-          path = ./shell;
-          description = "Shell script development environment";
-        };
-
-        zig = {
-          path = ./zig;
-          description = "Zig development environment";
-        };
-
-        # Aliases
-        # rt = rust-toolchain;
       };
-    };
 }
--- rust-toolchain/flake.nix
+++ rust-toolchain/flake.nix
@@ -9,33 +9,45 @@
     };
   };
 
-  outputs = { self, nixpkgs, rust-overlay }:
+  outputs =
+    {
+      self,
+      nixpkgs,
+      rust-overlay,
+    }:
     let
       overlays = [
         rust-overlay.overlays.default
-        (final: prev: {
-          rustToolchain =
-            prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml;
-        })
+        (final: prev: { rustToolchain = prev.rust-bin.fromRustupToolchainFile ./rust-toolchain.toml; })
       ];
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
-      forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit overlays system; }; });
-    in {
-      devShells = forEachSupportedSystem ({ pkgs }: {
-        default = pkgs.mkShell {
-          packages = with pkgs; [
-            rustToolchain
-            openssl
-            pkg-config
-            cargo-deny
-            cargo-edit
-            cargo-watch
-            rust-analyzer
-          ];
-        };
-      });
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        nixpkgs.lib.genAttrs supportedSystems (
+          system: f { pkgs = import nixpkgs { inherit overlays system; }; }
+        );
+    in
+    {
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              rustToolchain
+              openssl
+              pkg-config
+              cargo-deny
+              cargo-edit
+              cargo-watch
+              rust-analyzer
+            ];
+          };
+        }
+      );
     };
 }
